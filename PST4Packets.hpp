102344f571e165565b29a1397c1c826e5e17a7dd
Arthur Brainville <ybalrid@ybalrid.info>
2017-02-02 00:45:09 +0100

    Add the header that will define the packets

---
+++ PST4Packets.hpp
@@ -0,0 +1,9 @@
+namespace PST4
+{
+    enum ID_PST4_MESSAGE_TYPE
+    {
+        //TODO define RakNet messages here
+    };
+
+    //TODO define struct for the packet here, or bitstream builder functions here
+}
